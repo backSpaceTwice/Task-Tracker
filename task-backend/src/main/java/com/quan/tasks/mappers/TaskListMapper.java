d5accd3a010ba9700565b10435f3f8ba234acf33
backSpaceTwice <98787680+Lil-Bapi@users.noreply.github.com>
2026-02-25 19:28:59 -0800

    feat: add TaskListMapper interface and implementation

--- task-backend/src/main/java/com/quan/tasks/mappers/TaskListMapper.java
+++ task-backend/src/main/java/com/quan/tasks/mappers/TaskListMapper.java
@@ -1,4 +1,12 @@
 package com.quan.tasks.mappers;
 
+import com.quan.tasks.domain.dto.TaskDto;
+import com.quan.tasks.domain.dto.TaskListDto;
+import com.quan.tasks.domain.entities.TaskList;
+
 public interface TaskListMapper {
+
+    TaskList fromDto(TaskListDto taskListDto);
+
+    TaskListDto toDto(TaskList taskList);
 }
