af8657168ea559d5abdba8e05dede01fe21600e1
backSpaceTwice <98787680+Lil-Bapi@users.noreply.github.com>
2026-03-13 20:26:57 -0700

    feat(backend): add task creation endpoints and service logic

---
+++ task-backend/src/main/java/com/quan/tasks/controllers/TaskController.java
@@ -0,0 +1,41 @@
+package com.quan.tasks.controllers;
+
+import com.quan.tasks.domain.dto.TaskDto;
+import com.quan.tasks.domain.entities.Task;
+import com.quan.tasks.mappers.TaskMapper;
+import com.quan.tasks.services.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/task-lists/{task_list_id}/tasks")
+@CrossOrigin(origins = "http://localhost:5173")
+public class TaskController {
+
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+    @GetMapping
+    public List<TaskDto> listTasks(@PathVariable("task_list_id") UUID taskListId) {
+        return taskService.listTasks(taskListId)
+                .stream()
+                .map(taskMapper::toDto)
+                .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(@PathVariable("task_list_id") UUID taskListId, @RequestBody TaskDto taskDto) {
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+        return  taskMapper.toDto(createdTask);
+    }
+}
